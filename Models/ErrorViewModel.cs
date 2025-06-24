129fdcbbbe5650dada782a76478882a6fbc8aed0
hih0lp <mrlibobo@yandex.ru>
2025-06-24 18:03:51 +0300

    -

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERP_Proflipper_WorkspaceService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
