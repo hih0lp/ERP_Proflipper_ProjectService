68e82de9dd995dd6d85b6164b7dc81240b92dbfd
Clever <sanya.veselkov.811@gmail.com>
2025-06-25 17:48:01 +0300

    db context and project model

---
+++ ProjectsDB.cs
@@ -0,0 +1,20 @@
+﻿using ERP_Proflipper_WorkspaceService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP_Proflipper_WorkspaceService
+{
+    public class ProjectsDB : DbContext
+    {
+        public ProjectsDB()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ProjectModel> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
