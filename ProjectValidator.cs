8a6fa27ed8193cebd858fb744d616e1e227a8261
hih0lp <mrlibobo@yandex.ru>
2025-10-06 13:51:13 +0300

    -

--- ProjectValidator.cs
+++ ProjectValidator.cs
@@ -8,14 +8,7 @@ namespace ERP_Proflipper_ProjectService
     {
         public ProjectValidator()
         {
-            RuleFor(project => project.Name).NotEmpty();
             RuleFor(project => project.NowStatus).NotEmpty();
-            RuleFor(project => project.Area).NotEqual(0);
-            RuleFor(project => project.Price).NotEqual(0);
-            RuleFor(project => project.Location).NotEmpty();
-            RuleFor(project => project.Condition).NotEmpty();
-            RuleFor(project => project.CreatedAt).NotEmpty();
-            RuleFor(project => project.UpdatedAt).NotEmpty();
         }
     }
 }
